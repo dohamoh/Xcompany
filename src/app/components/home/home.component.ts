d39f2276e50ff16ed8ae805cabff6a9b11d1c029
dohamoh <doh.mohammed@gmail.com>
2023-04-11 19:04:09 +0300

    doha2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,8 +11,11 @@ export class HomeComponent {
      const rotation = scrollPosition * 0.1; // adjust this value as needed
      const image = document.querySelector('.bgImg2') as HTMLImageElement;
      const image2 = document.querySelector('.bgImg3') as HTMLImageElement;
+     const image3 = document.querySelector('.bgImg6') as HTMLImageElement;
+     const image4 = document.querySelector('.bgImg9') as HTMLImageElement;
      image.style.transform = `rotate(${rotation}deg)`;
      image2.style.top = `${scrollPosition * -0.10}px`; // adjust this value as needed
-    //  image2.style
+     image3.style.left = `${scrollPosition * 0.10}px`;
+     image4.style.transform = `rotate(${rotation}deg)`;
   }
 }
