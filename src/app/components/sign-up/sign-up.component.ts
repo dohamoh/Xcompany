2ce138b2a78d40e12fdc5dbe0396c4b942505cb2
dohamoh <doh.mohammed@gmail.com>
2023-05-15 17:05:24 +0300

    final-d-1

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -24,7 +24,6 @@ export class SignUpComponent {
   onSubmit(data: any) {
     this.Auth.signUp(this.registerForm.value).subscribe(
       (data: any) => {
-
         if (data.message == 'added successfully') {
           this.router.navigate(['/logIn'])
         }
